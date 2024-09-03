dc4ab3174d5c30f4e50f9283a8220acc3109ffe6
zeeshan <zeeshantt04@gmail.com>
2024-09-03 10:37:23 +0500

    first commit

---
+++ loop.js
@@ -0,0 +1,31 @@
+// const word ="javascript";
+// // for (let i = 0; i < word.length; i ++){
+// //     console.log(word[i]);
+// // }
+// for (let i= a.length-1; i<=0;i--){
+//     console.log(a[i])
+// }
+// function isplaindrome(str){
+//     let reverse_str= "";
+//     for (let i = str.length-1;i>=0;i--){
+//         reverse_str=reverse_str+str[i]
+//     }
+//     if(str=== reverse_str){
+//         console.log("its a plaindrome word")
+//     }else{
+//         console.log("its  not a plaindrome word")
+//     }
+
+// }
+// isplaindrome("madam")
+// isplaindrome("javascript")
+// isplaindrome("racecar")
+let i =0;
+// let str ="shumaila";
+let arr=["sunday","monday","tuesday"]
+while(i<arr.length){
+    console.log(arr[i])
+    i++
+}
+
+
